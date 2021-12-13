ac7b64e72622dc19bdce250cf0f24710126a7913
Lothering0 <danil2005232@gmail.com>
2021-12-13 14:56:49 +0300

    Moved server side into another dir

---
+++ users.ts
@@ -0,0 +1,40 @@
+interface User {
+  name: string
+  room: string | number
+  color: string
+  userId?: string
+}
+
+class Users {
+  public users: User[]
+
+  constructor() {
+    this.users = []
+  }
+
+  add(user: User) {
+    this.users.push(user)
+  }
+
+  get(id: string) {
+    return this.users.find(user => user.userId === id)
+  }
+
+  remove(id: string) {
+    const user = this.get(id)
+
+    if (user) {
+      this.users = this.users.filter(user => user.userId !== id)
+    }
+
+    return user
+  }
+
+  getByRoom(room: string) {
+    return this.users.filter(user => user.room === room)
+  }
+}
+
+module.exports = function() {
+  return new Users()
+}
